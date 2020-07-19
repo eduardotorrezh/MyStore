e130c3073172c82b27585ff65dcd293c0015dbf9
WilliamsDJGarcia <43178740+WilliamsDJGarcia@users.noreply.github.com>
2020-07-19 11:42:54 -0500

    Test
    last_login

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -5,11 +5,29 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 
 class PassportController extends Controller
 {
+
+    public $maxAttempts=3;
+    public $decayMinutes=2;
+
+    // $variable = $this->limiter()->hit($this->throttleKey($request));
+
     public function login(Request $request)
     {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            Log::alert('Se ha alcanzado el límite de intentos máximos que son: '.$this->maxAttempts.' por parte de: '.$request->email);
+            $this->fireLockoutEvent($request);
+            return response()->json([
+                'Se ha rebasado el número de intentos' => $this->maxAttempts(),
+                'Intento actual' => $this->limiter()->hit($this->throttleKey($request)),
+                'Tiempo de espera para próximo intento' => $this->decayMinutes().' minutos',
+                'Intentando accesar desde el correo' => $request->email,
+            ],429);
+        }
 
         $credentials = $request->only('email', 'password');
 
@@ -19,6 +37,8 @@ class PassportController extends Controller
         ]);
 
         if ($validator->fails()) {
+            Log::channel('single')->info('Datos incorrectos por parte de: '.$request->email. ' contraseña: '.$request->password);
+
             return response()->json([
                 'message' => 'Por favor, llenar los campos correctamente',
                 'errors' => $validator->errors()
@@ -30,15 +50,21 @@ class PassportController extends Controller
             $status = $user->status;
 
             if(!$status){
+                Log::channel('single')->info('No se ha encontrado el usuario '.$request->email);
+
                 return response()->json([
                     'message' => 'No se encontro usuario o ha sido eliminado'
                 ],404);
             }
 
             $token = $user->createToken('users')->accessToken;
+            Log::channel('single')->info('Ha iniciado sesión con éxito '.$request->email);
+
             return response()->json(['success' => true, 'token' => $token],
             200);
         } else
+            Log::channel('single')->info('Datos incorrectos por parte de '.$request->email);
+
             return response()->json(['res' => false, 'message' => "Usuario o contraseña incorrectos"],
             404);
     }
@@ -48,6 +74,7 @@ class PassportController extends Controller
         $user->tokens->each(function ($token, $key){
             $token->delete();
         });
+        Log::channel('single')->info('Usuario '.$request->email. ' cerro sesión');
         return response()->json(['res' => true, 'message' => "Hasta luego"],200);
     }
 
---
+++ app/Listeners/SuccessfulLogin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use Laravel\Passport\Events\AccessTokenCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  AccessTokenCreated  $event
+     * @return void
+     */
+    public function handle(AccessTokenCreated $event)
+    {
+        $timeDate = new \DateTime ;
+        // print("TIEMPO ACTUAL "+$timeDate);
+        $event->user->last_login = $timeDate;
+        $event->user->save();
+    }
+}
